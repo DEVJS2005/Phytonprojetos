878a60225ddab5e2e48dbcdd8624e7fc39e798d4
DEVJS2005 <79548150+DEVJS2005@users.noreply.github.com>
2021-03-15 17:41:35 -0300

    Sem perdas

--- PHYTON_projetos/conversordeMedidas.py
+++
@@ -1,4 +0,0 @@
-print('___________Olá!bem vindo ao conversor de medidas.___________')
-print('1-KM;   2-HM;\n3-DM;   4-M;\n5-DM;   6-CM;\n    7-MM')
-medida1 = int(input('escolha uma das medidas:'))
-sigla = str
