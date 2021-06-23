bb1fd72c9730bfde6916c051b908dec70f02a754
DEVJS2005 <79548150+DEVJS2005@users.noreply.github.com>
2021-06-23 09:32:07 -0300

    Desafio #1
    Primeiro desafio concluído com sucesso e com algumas implementações no código!!

---
+++ Desafios/Chute_numero.py
@@ -0,0 +1,34 @@
+import random
+import os
+
+
+numerof = random.randint(0,100)
+
+print("_____Chute o numero entre 0 e 100_____")
+op= False
+while op == False:
+    print("_____Chute o numero entre 0 e 100_____")
+    numeroI = int(input("Chute um numero:"))
+    if numeroI < numerof:
+        print("Chutou baixo... ;)")
+    elif numeroI > numerof:
+        print("Chutou alto... ;)")
+    elif numerof == numeroI:
+        print("Parabéns!! Você acertou. ;)")
+    else:
+        print("Só chute numeros entre 0 e 100")
+    op = input("Deseja tentar novamente?(y/n):")
+    if op == "y":
+        op = False
+        os.system("cls")
+    elif op == "n":
+        op = True
+        os.system("cls")
+    else :
+        print("Escolha entre y/n. ;)")
+        os.system("cls")
+
+
+
+
+
