1e4d9581adcef1f98594a398e3bd8a5765e2c4d6
DEVJS2005 <79548150+DEVJS2005@users.noreply.github.com>
2021-06-27 08:47:53 -0300

    Conversor de medidas com interface
    Conversor de medidas com interface gráfica finalizado.

---
+++ Desafios/conversor/conversordeMedidas_GUI.py
@@ -0,0 +1,62 @@
+from typing import Text
+from PyQt5 import  uic,QtWidgets
+
+#funções de calculo;
+def mult1000(x):
+        x *= 1000
+        msg.setWindowTitle("Conversão realizada!")
+        msg.setText("Resultado:{}".format(x))
+        msg.exec()
+def mult1000000(x):
+        x = x * 1000000
+        msg.setWindowTitle("Conversão realizada!")
+        msg.setText("Resultado:{}".format(x))
+        msg.exec()
+def div1000(x):
+        x /= 1000
+        msg.setWindowTitle("Conversão realizada!")
+        msg.setText("Resultado:{}".format(x))
+        msg.exec()
+def div1000000(x):
+        x /= 1000000
+        msg.setWindowTitle("Conversão realizada!")
+        msg.setText("Resultado:{}".format(x))
+        msg.exec()
+#Função principal; 
+def funcao_principal():
+        linha1 = float(formulario.quant.text())
+        print("Quantidade:{}".format(linha1))
+        if formulario.kg.isChecked and formulario.G_check.isChecked():
+                mult1000(linha1)
+        elif formulario.kg.isChecked and formulario.mg_check.isChecked():
+                mult1000000(linha1)
+        elif formulario.g.isChecked and formulario.kg_check.isChecked():
+                div1000(linha1)
+        elif formulario.g.isChecked and formulario.mg_check.isChecked():
+                div1000(linha1)
+        elif formulario.mg.isChecked and formulario.kg_check.isChecked():
+                div1000000(linha1)
+        elif formulario.mg.isChecked and formulario.G_check.isChecked():
+                div1000(linha1)
+#controle de erros;
+        elif formulario.kg.isChecked and formulario.kg_check.isChecked():
+                erro.setWindowTitle("ERRO")
+                erro.setText("Você não pode converter para a mesma unidade. ;)")
+                erro.exec()
+        elif formulario.g.isChecked and formulario.G_check.isChecked():
+                erro.setWindowTitle("ERRO")
+                erro.setText("Você não pode converter para a mesma unidade. ;)")
+                erro.exec()
+        elif formulario.mg.isChecked and formulario.mg_check.isChecked():
+                erro.setWindowTitle("ERRO")
+                erro.setText("Você não pode converter para a mesma unidade. ;)")
+                erro.exec()
+        formulario.quant.setText("")
+#Funções padronizadas;
+app=QtWidgets.QApplication([])
+msg = QtWidgets.QMessageBox()
+erro = QtWidgets.QMessageBox
+formulario=uic.loadUi("formulario2.ui")
+formulario.convert.clicked.connect(funcao_principal)
+formulario.show()
+app.exec()
